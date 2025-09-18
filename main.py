8277d64986a083ee078bc94f0e7c39f67a74b0a0
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-09-18 20:08:30 +0200

    add def chat to work with chat widget

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 import os
+
+from rag import generate_content
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
 async def read_root():
@@ -13,4 +17,20 @@ async def read_root():
 
 @app.get("/api/hello")
 async def get_message():
-    return {"message": "Hello from FastAPI 🎉"}
+    return {"message": "Hello from ReDI 🎉"}
+
+
+class ChatRequest(BaseModel):
+    question: str
+    history: list[dict] = []
+
+
+@app.post("/api/chat")
+async def chat(req: ChatRequest):
+    # Mock RAG: just echo the question
+    # return {"answer": f"You asked: '{req.question}'. (RAG coming soon 🤖)"}
+    messages = req.history + [
+        {"role": "user", "content": req.question}
+    ]
+    answer = generate_content(messages)
+    return {"answer": answer}
